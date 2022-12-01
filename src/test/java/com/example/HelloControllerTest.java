53fa2742a7d026ea7a9150bdaad96d644fccf299
jsanders-certipath <119342598+jsanders-certipath@users.noreply.github.com>
2022-12-01 08:58:15 -0500

    simplify

--- src/test/java/com/example/HelloControllerTest.java
+++ src/test/java/com/example/HelloControllerTest.java
@@ -1,29 +1,22 @@
 package com.example;
 
 import com.jayway.restassured.RestAssured;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = App.class)
-@WebAppConfiguration
-@IntegrationTest({"server.port:0",
-        "spring.datasource.url:jdbc:h2:mem:demo;DB_CLOSE_ON_EXIT=FALSE"})
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
-    @Value("${local.server.port}")
+
+    @LocalServerPort
     int port;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         RestAssured.port = port;
     }
@@ -34,14 +27,4 @@ public class HelloControllerTest {
                 .body(is("Hello World!"));
     }
 
-    @Test
-    public void testCalc() throws Exception {
-        given().param("left", 100)
-                .param("right", 200)
-                .get("/calc")
-                .then()
-                .body("left", is(100))
-                .body("right", is(200))
-                .body("answer", is(300));
-    }
 }
